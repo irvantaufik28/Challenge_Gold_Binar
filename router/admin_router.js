afae8d9ada29084e0632c7a60e960d263cd7829e
irvan taufik <irvantaufik28@gmail.com>
2022-08-22 21:38:41 +0700

    refactory code

--- router/admin_router.js
+++ router/admin_router.js
@@ -1,25 +1,27 @@
 const express = require("express")
 const router = express.Router()
-const admin = require('../controller/admin_item_controller')
+const admin_item = require('../controller/admin_item_controller')
+const admin_order= require('../controller/admin_order_controller')
 const Validation_product = require('../validation/product/product.validation')
 const Validation_category = require('../validation/category/category.validation')
 
 
 // product
-router.post('/product/add',Validation_product.createProduct, admin.addProduct)
-router.put('/product/update/:id',Validation_product.createProduct, admin.editProduct)
-router.delete('/product/delete/:id', admin.delete)
+router.post('/product/add',Validation_product.createProduct, admin_item.addProduct)
+router.put('/product/update/:id',Validation_product.createProduct, admin_item.editProduct)
+router.delete('/product/delete/:id', admin_item.delete)
 
 
 
 // category
-router.post('/category/add',Validation_category.createCategory ,admin.addCategory)
-router.delete('/category/delete/:id', admin.destroyCategory)
+router.post('/category/add',Validation_category.createCategory ,admin_item.addCategory)
+router.delete('/category/delete/:id', admin_item.destroyCategory)
 
 
 // order
 
-router.patch('/order/update', admin.changeStatusOrder)
-router.get('/order', admin.getCompletedOrder)
+router.patch('/order/update', admin_order.changeStatusOrder)
+router.get('/order/:id', admin_order.getCompletedOrder)
+router.get('/order/', admin_order.getCompletedOrder)
 
 module.exports= router
