451d3e44b6a43626dc7e316aff02488a8eb42e40
irvan taufik <irvantaufik28@gmail.com>
2022-08-24 09:01:09 +0700

    add new postman

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const bodyParser = require('body-parser')
 const app = express()
 
 const host = 'localhost'
@@ -10,15 +9,10 @@ const authRouter = require('./router/auth_router')
 const adminRouter = require('./router/admin_router')
 const customerRouter = require('./router/customer_router')
 
-// app.use(bodyParser.json())
+
 app.use(express.json())
-// app.use(bodyParser.urlencoded({extended :true}))
 app.use(morgan('combined'))
 
-
-// const db = require('./models/index') 
-
-// db.sequelize.sync({force:false})
 
 app.get('/',(req, res)=>{
     res.json({
